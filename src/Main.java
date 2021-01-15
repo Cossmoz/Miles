dcb944be467039910c884bf3a03c444d378ca193
Constantin Zamotaev <cossx@yahoo.com>
2021-01-15 23:46:21 +0400

    project added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1024;
+        int totalFlights = 4;
+        int totalMiles = (ticketPrice * totalFlights) / 20; //
+        System.out.println("Всего бонусных миль: " + totalMiles);
+    }
+}
