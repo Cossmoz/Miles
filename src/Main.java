b7f7d72d8dcd4ad9863921d2e01caada185b1675
Constantin Zamotaev <cossx@yahoo.com>
2021-01-15 23:51:48 +0400

    deleting comments

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 1024;
         int totalFlights = 4;
-        int totalMiles = (ticketPrice * totalFlights) / 20; //
+        int totalMiles = (ticketPrice * totalFlights) / 20;
         System.out.println("Всего бонусных миль: " + totalMiles);
     }
 }
